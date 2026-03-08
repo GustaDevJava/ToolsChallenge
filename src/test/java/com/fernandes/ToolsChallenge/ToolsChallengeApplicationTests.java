68ed67adfedcc89c4e05d9c6efe03bb0f5e0ae1f
Gustavo <gusta.estudos.java@gmail.com>
2026-03-08 08:14:27 -0300

    Primeiro commit

---
+++ src/test/java/com/fernandes/ToolsChallenge/ToolsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandes.ToolsChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
